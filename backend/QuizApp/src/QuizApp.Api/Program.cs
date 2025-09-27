3ed1badd6ff14c0b93d37b76a1b1eaea64afde56
Martin.Muharemovic <makimuh02@gmail.com>
2025-09-27 15:03:50 +0200

    Removed unused OpenApi (swagger is used completely).

--- backend/QuizApp/src/QuizApp.Api/Program.cs
+++ backend/QuizApp/src/QuizApp.Api/Program.cs
@@ -3,15 +3,10 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
